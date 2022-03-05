9046a2f1c3beb9121199087887df715086f01e0a
Chathura-ce <chathu.eac@gmail.com>
2022-03-05 22:36:07 +0530

    nprogress added

--- resources/js/app.js
+++ resources/js/app.js
@@ -15,9 +15,26 @@ Vue.use(VueRouter)
 import App from './components/App'
 import routes from './routes'
 
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+const router = new VueRouter(routes)
+router.beforeResolve((to, from, next) => {
+    // If this isn't an initial page load.
+    if (to.name) {
+        // Start the route progress bar.
+        NProgress.start()
+    }
+    next()
+})
+
+router.afterEach((to, from) => {
+    // Complete the animation of the route progress bar.
+    NProgress.done()
+})
+
 
 const app = new Vue({
     el: '#app',
     components: {App},
-    router: new VueRouter(routes)
+    router: router
 });
